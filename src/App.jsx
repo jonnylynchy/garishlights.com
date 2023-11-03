5ee1a553261f851cfea02d3125ee1638e53f410e
Jon Lynch <jonnylynchy@gmail.com>
2023-11-02 20:02:38 -0500

    added initial files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import garishLights from '/images/virginia-tacky-lights-tour-richmond-LIGHTS1215.webp'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Garish Lights!</h1>
+      <h3>Coming soon to a Lantana Subdivision near you!</h3>
+      <img src={ garishLights } className="logo" alt="React logo" />
+    </>
+  )
+}
+
+export default App
